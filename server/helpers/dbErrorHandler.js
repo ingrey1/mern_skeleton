524fc6591788fb52abd9a9d733632ae1524301d9
Jason Mortensen <ingrey1@gmail.com>
2019-04-21 05:47:36 -0700

    Added mongoose error handler helper

---
+++ server/helpers/dbErrorHandler.js
@@ -0,0 +1,46 @@
+// handling mongoose errors
+const getErrorMessage = err => {
+  let message = '';
+  if (err.code) {
+    switch (err.code) { // not thrown by mongoose validator violation
+      case 11000:
+
+      case 11001:
+        message = getUniqueErrorMessage();
+        break;
+      default:
+        message = 'something went wrong';
+    }
+  } else { // thrown by mongoose validator violation
+    for (let errName in err.errors) {
+      if (err.errors[errName].message) {
+        message = err.errors[errName].message;
+      }
+    }
+  }
+  return message;
+};
+
+const getUniqueErrorMessage = (err) => {
+
+
+	let output
+
+	try {
+
+		let fieldName = err.message.substring(err.message.lastIndexOf('.$') + 2, err.message.lastIndexOf('_1'))
+		output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + 'already exists'
+
+	} catch (ex) {
+
+		output = 'Unique Field Already Exists'
+
+	}
+
+	return output
+
+}
+
+
+
+export default { getErrorMessage };
